ea03972169a85fe6b0eac1624b0c6148594a30dc
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-10 21:02:06 +0100

    Update bash.zsh

--- bash.zsh
+++ bash.zsh
@@ -5,3 +5,8 @@ hatch run lint:all     # for both
 
 # Format
 hatch run lint:fmt
+# Run against kaggle.com
+hatch run test:prod
+
+# Run against a local web server (Kaggle engineers only)
+hatch run test:local
