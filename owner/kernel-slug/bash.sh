84214274dadacc9fcf91973102800db663a92a6c
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-10 20:57:16 +0100

    Enhance bash script with Kaggle commands
    Added commands to pull a kernel and list datasets and competitions.

--- owner/kernel-slug/bash.sh
+++ owner/kernel-slug/bash.sh
@@ -1,1 +1,8 @@
->_kaggle kernels pull auraecosystem/save-model99d1ea7037
+>_
+kaggle kernels pull auraecosystem/save-model99d1ea7037
+hatch shell
+
+# Inside the shell, you can run many commands
+kaggle datasets list
+kaggle competitions list
+...
